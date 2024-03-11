9090ba9fb66f220b8a0596b4b461be9082a403c1
ZANAGLIA PAUL <paulzanaglia06@gmail.com>
2024-03-11 20:29:28 +0100

    first implementation without testing

--- image_trater/Slice.py
+++ image_trater/Slice.py
@@ -1,18 +1,62 @@
+from image_trater.Coeff_Strategy import Coeff_Strategy
+from image_trater.Donati import Donati
+from image_trater.Utility import compression_coefficient
+from PIL import Image
+
+
 class Slice:
+    default_pixel = None
 
     def __init__(self):
         pass
 
-    def generate_image(self, src, dst):
+    def generate_image(self, slice_info, src, dst, debug=False):
         """
         This function takes an image from src
         and saves the results in dst
-
         Args:
         src: str
             The source of the image
         dst: str
             The destination of the image
         """
-        pass
+        p, q = slice_info
+        image = Image.open(src)
 
+        # Convert in RGB
+        image = image.convert("RGB")
+
+        # Get pixels
+        pixels = list(image.getdata())
+        res = [None for _ in range(0, len(pixels))]
+
+        donati = Donati(p, q)  # y = px + q
+        strategy = Coeff_Strategy()
+
+        yy, xx = image.size
+
+        for y in range(yy):
+            for x in range(xx):
+                pixel = image.getpixel((x, y))
+
+                if debug:
+                    print(f"Coordonnées du pixel : ({x}, {y}), Valeur du pixel : {pixel}")
+
+                if donati.is_point_on(x, y):
+                    index = x if q < 1 else y
+
+                    if res[index] is None:
+                        res[index] = pixel
+                    else:
+                        current_pixel = res[index]
+                        c1, c2 = compression_coefficient(current_pixel, pixel, donati.get_distance_from_point,
+                                                         strategy.eval_coeff_by_max_dist)
+                        res[index] = (current_pixel, pixel, c1, c2)
+
+            res = [pixel if pixel is not None else self.default_pixel for pixel in res]
+
+            # Put the pixels into the image
+            image.putdata(pixels)
+
+            # Save the image
+            image.save(dst)
---
+++ image_trater/Utility.py
@@ -0,0 +1,7 @@
+def compression_coefficient(pixel1, pixel2, eval_func, strategy):
+    x1, y1 = pixel1
+    x2, y2 = pixel2
+    d1 = eval_func(x1, y1)
+    d2 = eval_func(x2, y2)
+    c1, c2 = strategy(d1, d2)
+    return c1, c2
