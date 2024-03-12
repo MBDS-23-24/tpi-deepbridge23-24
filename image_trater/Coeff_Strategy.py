647f35a6848d2469c1bc467fb5e26b86bb493039
AbdenourAchouri <achouriabdenour.98@gmail.com>
2024-03-12 15:28:23 +0100

    Fonction ajoutée

--- image_trater/Coeff_Strategy.py
+++ image_trater/Coeff_Strategy.py
@@ -10,3 +10,13 @@ class Coeff_Strategy:
 
     def eval_coeff_by_min_dist(self, d1, d2):
         self.eval_coeff_by_max_dist(d2, d1)
+
+    def pourcentage_pixel(self, d1, d2):
+
+        d = d1 + d2
+        c1 = d - (d1 / d)
+        c2 = d - (d2 / d)
+
+        return c1, c2
+
+
