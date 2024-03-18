357d701e54eed337e0b8e1153f195dc3caab59ae
ZANAGLIA PAUL <paulzanaglia06@gmail.com>
2024-03-18 19:04:57 +0100

    remove todo on main

--- image_trater/main.py
+++ image_trater/main.py
@@ -3,13 +3,11 @@ from Slice import Slice
 import os
 
 
-def run(src, dest, slice_info=(1.5, -.33)):
+def run(src, dest, slice_info=(1.5, -.33)):  # (p, q) values for line equation (px + q = y)
     _slice = Slice()
     return _slice.generate_image(slice_info, src, dest, debug=True)
 
 
 if __name__ == "__main__":
-    # value defined for straight line equation : y = px + q
-    # TODO: define p and q
     path = os.getcwd()
     run(f"{path}/examples/tests/line2D/3_div_2x", f"{path}/examples/tests/line2D/results/3_div_2x.png")
